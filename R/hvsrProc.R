57700b7b256148bde525e2c5159a2171c7721b1a
wltcwpf <wltcwpf@gmail.com>
2021-06-14 22:55:12 -0700

    add package help

---
+++ R/hvsrProc.R
@@ -0,0 +1,26 @@
+#' HVSR Processing Package
+#'
+#' This package is developed for processing and calculating
+#' HVSR from noise-based and earthquake-based time series data.
+#'
+#' @docType package
+#'
+#' @name hvsrProc-package
+#' @aliases hvsrProc hvsrProc-package
+#'
+#' @note There are 17 hvsrProc functions:
+#' \code{ang_pga_rotd50_calc, bw_pass, bw_resp, fas_cal, fd_plt_select,
+#' hv_proc, hvsr_win_calc, ko_smooth, log10_ticks, logspace,
+#' mean_removal, parzen_smooth, plot_log10, pre_proc,
+#' sta_lta_calc, taper, td_plt_select}
+#'
+#' Of which \code{hv_proc} is main function.
+#'
+#' @examples
+#' ## Not run:
+#' ##   See examples in the help files for all functions.
+#' ## End(Not run)
+#'
+#' @author Pengfei Wang
+NULL
+#> NULL
